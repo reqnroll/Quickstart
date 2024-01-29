09b7df6d1d50de9f35bf8bd805a831d49bf8d23a
Gaspar Nagy <gaspar.nagy@gmail.com>
2024-01-29 10:44:55 +0100

    automated steps

--- ReqnrollQuickstart.Specs/StepDefinitions/PriceCalculationStepDefinitions.cs
+++ ReqnrollQuickstart.Specs/StepDefinitions/PriceCalculationStepDefinitions.cs
@@ -3,27 +3,32 @@ namespace ReqnrollQuickstart.Specs.StepDefinitions;
 [Binding]
 public class PriceCalculationStepDefinitions
 {
+    private readonly PriceCalculator _priceCalculator = new();
+    private readonly Dictionary<string, int> _basket = new();
+    private decimal _calculatedPrice;
+
     [Given("the client started shopping")]
     public void GivenTheClientStartedShopping()
     {
-        throw new PendingStepException();
+        _basket.Clear();
+        _calculatedPrice = 0.0m;
     }
 
     [Given("the client added {int} pcs of {string} to the basket")]
     public void GivenTheClientAddedPcsOfToTheBasket(int quantity, string product)
     {
-        throw new PendingStepException();
+        _basket.Add(product, quantity);
     }
 
     [When("the basket is prepared")]
     public void WhenTheBasketIsPrepared()
     {
-        throw new PendingStepException();
+        _calculatedPrice = _priceCalculator.CalculatePrice(_basket);
     }
 
     [Then("the basket price should be ${float}")]
     public void ThenTheBasketPriceShouldBe(decimal expectedPrice)
     {
-        throw new PendingStepException();
+        Assert.AreEqual(expectedPrice, _calculatedPrice);
     }
 }
